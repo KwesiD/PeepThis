5cfea4802cfbbfc760ff1859f4e97cb0a73daf75
Kwesi <kfd233@nyu.edu>
2017-01-14 00:13:08 -0500

    Added a few method to the Display.py for a quick mockup. Adapted a few pygame methods in order to create the window.

--- Display/Display.py
+++ Display/Display.py
@@ -8,6 +8,9 @@ import random
 
 
 window = None
+window_width = 0
+window_height = 0
+
 def getWeather():
 	ip,location,status = IP.getLocation() #returns ip,(city,state,country,postal),status
 	if status != None:
@@ -17,6 +20,10 @@ def getWeather():
 	postal = location[3]
 	 #returns  condition,(current,high,low),(rain,snow,clouds,humidity,wind_speed),status
 	condition,temp,precipitation,status = Weather.getWeatherFromZip(postal)
+	if status != None:
+		##Send error message, do whatever, Idk
+		print(status)
+		exit(0)
 	return condition,temp,precipitation,status
 
 
@@ -26,10 +33,35 @@ def get_screen_resolution():
     return {'width': resolution[0], 'height': resolution[1]}
 
 def create_screen():
+	global window
+	global window_width
+	global window_height
 	dimensions = get_screen_resolution()
-	width = int(dimensions["width"])
-	height = int(dimensions["height"])
+	window_width = int(dimensions["width"])
+	window_height = int(dimensions["height"])
 	pygame.init()
-	window = pygame.display.set_mode((width,height))
+	window = pygame.display.set_mode((window_width,window_height))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (255,255,255))
+    return textSurface, textSurface.get_rect()
+
+def show_weather(condition,temp,precipitation,status):
+	global window_height
+	global window_width
+	print(window_height)
+	text = condition + "\n" + "Now: " + str(temp[0]) + "\n" + "Hi: " + str(temp[1]) + "\n" + "Lo: " + str(temp[2]) + "\n" +\
+	 str(precipitation[2])+ "\n" + str(precipitation[3]) + "\n" + "Winds at " + str(precipitation[4])
+	font = pygame.font.Font('freesansbold.ttf',20)
+	TextSurf, TextRect = text_objects(text, font)
+	TextRect.center = ((window_width/3),(window_height/5))
+	window.blit(TextSurf, TextRect)
+
+	pygame.display.update()
+
+	time.sleep(4)
+
 
 create_screen()
+condition,temp,precipitation,status = getWeather()
+show_weather(condition,temp,precipitation,status)
