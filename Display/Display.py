f6bc664e35b66cafa846dc6d279625c244dd1451
Kwesi <kfd233@nyu.edu>
2017-01-13 23:28:05 -0500

    Added a basic window generator. Also connected IP and Weather files. Updated To-do with some info for installation.

--- Display/Display.py
+++ Display/Display.py
@@ -1,2 +1,35 @@
+import IP
+import Weather
+import pygame
+import subprocess
+import time 
+import random
 
 
+
+window = None
+def getWeather():
+	ip,location,status = IP.getLocation() #returns ip,(city,state,country,postal),status
+	if status != None:
+		##Send error message, do whatever, Idk
+		print(status)
+		exit(0)
+	postal = location[3]
+	 #returns  condition,(current,high,low),(rain,snow,clouds,humidity,wind_speed),status
+	condition,temp,precipitation,status = Weather.getWeatherFromZip(postal)
+	return condition,temp,precipitation,status
+
+
+def get_screen_resolution():
+    output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+    resolution = output.split()[0].split(b'x')
+    return {'width': resolution[0], 'height': resolution[1]}
+
+def create_screen():
+	dimensions = get_screen_resolution()
+	width = int(dimensions["width"])
+	height = int(dimensions["height"])
+	pygame.init()
+	window = pygame.display.set_mode((width,height))
+
+create_screen()
